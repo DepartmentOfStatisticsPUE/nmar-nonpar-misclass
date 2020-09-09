2351897cd127f908d9737721e5c93a6827cc5f3b
Maciej <maciej.beresewicz@gmail.com>
2020-09-09 09:04:15 +0200

    cleaning

--- codes/2-simulation-study.jl
+++ codes/2-simulation-study.jl
@@ -106,34 +106,6 @@ model_z3_x3_pr = mapslices(softmax, fitted(model_z3_x3), dims =2)
 
 ## using all information (testing whether the method works)
 
-function nmar_npar(selection, target, calvars, totalvars, data::DataFrame)
-    
-    vars_all = unique(vcat([selection, calvars, totalvars, target]...))
-    vars_cal_tot = unique(vcat([calvars, totalvars]...))
-    vars_cal_tot = setdiff(vars_cal_tot, [target])
-
-    df_sampl = by(data, vars_all, n = selection => length)
-    df_sampl_obs = df_sampl[df_sampl[!, selection].== 1, :]
-    df_sampl_obs = @transform(groupby(df_sampl_obs, vars_cal_tot), p_hat = :n/sum(:n))
-    df_sampl_nonobs = by(df_sampl[df_sampl[!, selection] .== 0,:], vars_cal_tot, m = :n => sum)
-    df_sampl_obs = leftjoin(df_sampl_obs, df_sampl_nonobs, on = vars_cal_tot)
-    df_sampl_obs.O = 1
-    O_start = df_sampl_obs.O
-
-    for iter in 1:10000
-        df_sampl_obs = @transform(groupby(df_sampl_obs, totalvars), m_hat = :m .* :p_hat .* :O / sum(:p_hat .* :O))
-        df_sampl_obs = @transform(groupby(df_sampl_obs, calvars), O = sum(:m_hat) / sum(:n))
-        dif = sum((O_start - df_sampl_obs.O).^2)   
-        if (dif < sqrt(eps()))
-            println("Converged on interation: ", iter, " with diff equal to ", dif)
-            break
-        end
-        O_start = df_sampl_obs.O
-    end
-
-    return df_sampl_obs
-end 
-
 res = nmar_npar(:flag_sel, :y, :y, [:x1, :x2], df)
 
 hcat(
